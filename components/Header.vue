59d12a2c7b421b7c0668a6afaff6358bde4c9985
pedrotorchio <pedrotorchio@gmail.com>
2018-08-09 23:38:39 -0300

    header init

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,28 @@
 <template lang="pug">
     aside
-      img(src="http://via.placeholder.com/350x150")
+      img(:src="logoUrl" v-if="logoUrl")/
+    
+      nav
+        ul
+          li primeiro
+          li segundo
+          li terceiro
+          li quarto
+
+      h4.email
+
 </template>
 <script>
 
 export default {
-  
+  data: () => ({
+    logoUrl: false
+  }),
+  created() {
+    this.$api.getAppInfo()
+        .then(info => {
+          this.logoUrl = info.logo.main_url
+        });
+  }
 }
 </script>

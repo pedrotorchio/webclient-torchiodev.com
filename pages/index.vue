8a6bad72c4e3ecee9b3ddf4af4000ae9a2c79dc6
pedrotorchio <pedrotorchio@gmail.com>
2018-08-09 21:48:46 -0300

    cleaning up

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,11 @@
 <template lang="pug">
-  section.container
-    h1 scss
+  main
 </template>
+<style lang="stylus">
 
+</style>
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
-  }
+
 }
 </script>
-
-<style lang="stylus">
-padding = 16px;
-.container
-  h1
-    padding: padding
-    border: 1px solid black
-</style>
-

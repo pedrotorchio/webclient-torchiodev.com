91e25e145f4d4c12f1f9873096b18bd16905b29b
pedrotorchio <pedrotorchio@gmail.com>
2018-08-09 21:26:15 -0300

    installing and testing scss

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,6 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        webclient-torchiodev.com
-      </h1>
-      <h2 class="subtitle">
-        personal isomorphic webpage
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <h1>scss</h1>
   </section>
 </template>
 
@@ -32,34 +14,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+$padding: 15px;
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  h1 {
+    padding: $padding;
+    border: 1px solid black;
+  }
 }
 </style>
 

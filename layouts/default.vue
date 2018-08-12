c28a84f170a7304fd96a3fe627642000a0842e25
pedrotorchio <pedrotorchio@gmail.com>
2018-08-12 11:39:31 -0300

    removing default layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,3 @@
 <template lang="pug">
-  div
-    page-header(
-      :logo="logo"
-      :email="email"
-    )
-    nuxt
+  nuxt
 </template>
-
-<style lang="stylus">
-@import '~assets/styles/global';
-  
-#main-header
-  width: 200px;
-  height: 100%;
-  height: 100vh;
-  border: 1px solid black;
-div
-  border: 1px solid black;
-
-  width: 100%;
-  width: 100vw;
-
-</style>
-
-<script>
-import PageHeader from '~/components/header/Header';
-
-export default {
-  components: {
-    PageHeader
-  }
-}
-</script>

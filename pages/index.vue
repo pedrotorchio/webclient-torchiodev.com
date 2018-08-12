a963fef6a1a64133c4d220a2e8eba3fc3ba9651d
pedrotorchio <pedrotorchio@gmail.com>
2018-08-12 11:40:39 -0300

    bringing header to page definition

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,41 @@
 <template lang="pug">
   main
-    p {{ $api }}
+    page-header(
+      :logo="logo"
+      :email="email"
+    )
 </template>
 
+<style lang="stylus">
+@import '~assets/styles/global';
+  
+#main-header
+  width: 200px;
+  height: 100%;
+  height: 100vh;
+  border: 1px solid black;
+div
+  border: 1px solid black;
+
+  width: 100%;
+  width: 100vw;
+
+</style>
+
 <script>
+import PageHeader from '~/components/header/Header';
 
 export default {
   components: {
+    PageHeader
+  },
+  async asyncData({app}) {    
+    const info = await app.$api.getAppInfo();
 
+    return {
+      email: info.contact_email,
+      logo: info.logo
+    }
   }
 }
 </script>
-
-<style>
-</style>
-

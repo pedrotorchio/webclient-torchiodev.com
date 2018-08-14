8036a3ce85dd5d911c490e08c125e904b8b9037c
pedrotorchio <pedrotorchio@gmail.com>
2018-08-14 00:13:54 -0300

    removing vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
+    vendor: ["vuetify"],
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({

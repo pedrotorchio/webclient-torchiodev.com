49b4d5a77eb55dcc61ee3378478f2e4ea29e07f1
pedrotorchio <pedrotorchio@gmail.com>
2018-08-15 17:35:53 -0300

    including in body

--- components/body/Body.vue
+++ components/body/Body.vue
@@ -1,10 +1,14 @@
 <script>
+import HeroSection from './hero/HeroSection';
+
 export default {
-  
+  components: {
+    HeroSection
+  }  
 }
 </script>
-<template>
+<template lang="pug">
   <div>
-    
+    hero-section#hero
   </div>
 </template>

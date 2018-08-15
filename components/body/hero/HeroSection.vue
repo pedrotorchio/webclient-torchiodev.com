8246c6eb19c1059a0d6a2ce1595c663d1b9b44e9
pedrotorchio <pedrotorchio@gmail.com>
2018-08-15 17:35:45 -0300

    section initial creation

---
+++ components/body/hero/HeroSection.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  section
+    
+</template>
+<style lang="stylus" scoped>
+
+section
+  background-color: black;
+  height: 600px;
+
+</style>
+<script>
+import ImageElement from '~/components/utils/ImageElement';
+
+export default {
+  name: 'HeroSection',
+  components: {
+    ImageElement
+  }
+}
+</script>

d1ec0e15a0462fe09594a5350f27002b0fdb20bf
pedrotorchio <pedrotorchio@gmail.com>
2018-08-15 19:45:19 -0300

    fetching and inserting img and title

--- components/body/hero/HeroSection.vue
+++ components/body/hero/HeroSection.vue
@@ -1,12 +1,38 @@
 <template lang="pug">
   section
-    
+    image-element(
+      :image="image"
+    )
+    h1(v-text='title')
+
 </template>
 <style lang="stylus" scoped>
 
 section
-  background-color: #7f5a98;
+  background-color: #c09ad9;
   height: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .img
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: 1;
+  
+  h1
+    font-weight: 100;
+    text-align: right;
+    list-style: none;
+    font-size: 48px;
+    text-transform: uppercase;
+    letter-spacing: 0.5em;
+    color: white;
+    z-index: 2;
+
 
 </style>
 <script>
@@ -16,6 +42,23 @@ export default {
   name: 'HeroSection',
   components: {
     ImageElement
+  },
+  data: () => ({
+    image: null,
+    title: ''
+  }),
+  methods: {
+    async fetchData() {
+      const info = await this.$api.getAppInfo();
+      return info;
+    }
+  },
+  created() {
+    this.fetchData()
+        .then( info => {
+          this.image = info.main_image
+          this.title = info.title
+        });
   }
 }
 </script>

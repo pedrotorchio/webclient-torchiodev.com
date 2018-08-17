5ca34f6d9045b0d1aa28e270cbdddc2164205834
pedrotorchio <pedrotorchio@gmail.com>
2018-08-17 14:35:39 -0300

    including bio section

---
+++ components/body/Bio/index.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template lang="pug">
+  
+</template>
--- components/body/Body.vue
+++ components/body/Body.vue
@@ -1,21 +1,26 @@
 <script>
 import HeroSection from './Hero';
+import BioSection from './Bio';
 
 export default {
   components: {
-    HeroSection
+    HeroSection,
+    BioSection
   }  
 }
 </script>
 <template lang="pug">
   <div>
     hero-section#hero
+    bio-section#bio
   </div>
 </template>
 
 <style lang="stylus">
+
 section
   position: relative;
   overflow: hidden;
+
 
 </style>

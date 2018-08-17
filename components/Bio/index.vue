845d65336d20090aaaa53f8e8bb126264d15c934
pedrotorchio <pedrotorchio@gmail.com>
2018-08-17 16:36:04 -0300

    fetching avatar

--- components/Bio/index.vue
+++ components/Bio/index.vue
@@ -1,10 +1,43 @@
 <script>
+
 export default {
   
+  data: () => ({
+    avatar: null
+  }),
+  methods: {
+    async fetchData() {
+      const info = await this.$api.getAbout();
+      return info;
+    }
+  },
+  created() {
+    this.fetchData()
+        .then( info => {
+          this.avatar = info.avatar_image;
+        });
+  }
 }
 </script>
 
 <template lang="pug">
   section
-    
+    div.avatar
+      image-element(
+        :image='avatar'
+      )
 </template>
+
+<style lang="stylus" scoped>
+
+.avatar
+  width: 300px;
+  position: relative;
+  border-radius: 50%;
+  padding: 20px;
+  background: grey;
+  
+  &:before
+    padding-bottom: 100%;
+
+</style>

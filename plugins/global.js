eb02b0abe7b2e5cce49891f37f098c2cc1178cea
pedrotorchio <pedrotorchio@gmail.com>
2018-08-19 11:31:11 -0300

    temp fixing external import of module using internal module folder

--- plugins/global.js
+++ plugins/global.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import { ResponsiveImageWrapper as ImageElement } from "vue-responsive-img";
-// import SectionContainer from '~/components/Section';
-import SectionContainer from 'vue-content-wrapper';
+import SectionContainer from '~/modules/ContentWrapper';
+// import SectionContainer from 'vue-content-wrapper';
 
 Vue.component("image-element", ImageElement);
 Vue.component("section-container", SectionContainer);
